9d08482fb718a3dbb00ef935f7b63d18995d7df5
cmparsons <christianp130@gmail.com>
2017-09-15 21:25:34 -0500

    Change coding format of curly braces. Add assignment name in header comments

--- hw1/hivemind.cpp
+++ hw1/hivemind.cpp
@@ -1,5 +1,6 @@
 /*
 * @author: Christian Parsons
+* @assignment: HW #1
 * @file: hivemind.cpp
 * @section: B
 * @due: September 8, 2017
@@ -53,8 +54,7 @@ bool is_isolated(
 // post:        Distance between (x_1, y_1) and (x_2, y_2) is returned
 double get_distance(const int x_1, const int y_1, const int x_2, const int y_2);
 
-int main()
-{
+int main() {
   int num_test_cases = 0;
   int num_rows = 0;
   int num_columns = 0;
@@ -63,8 +63,7 @@ int main()
 
   cin >> num_test_cases;
 
-  for (int i = 0; i < num_test_cases; i++)
-  {
+  for (int i = 0; i < num_test_cases; i++) {
     num_rad_robots = 0;
     cin >> num_rows >> num_columns;
 
@@ -84,21 +83,18 @@ int main()
   return 0;
 }
 
-int ** allocate_grid(const int num_rows, const int num_cols)
-{
+int ** allocate_grid(const int num_rows, const int num_cols) {
   int **grid = new int *[num_rows];
-  for (int i = 0; i < num_rows; i++)
-  {
+
+  for (int i = 0; i < num_rows; i++) {
     grid[i] = new int[num_cols];
   }
 
   return grid;
 }
 
-void deallocate_grid(int ** grid, const int num_rows)
-{
-  for (int i = 0; i < num_rows; i++)
-  {
+void deallocate_grid(int ** grid, const int num_rows) {
+  for (int i = 0; i < num_rows; i++) {
     delete[] grid[i];
   }
   delete[] grid;
@@ -107,12 +103,9 @@ void deallocate_grid(int ** grid, const int num_rows)
   return;
 }
 
-void insert_grid_data(int **grid, const int num_rows, const int num_cols)
-{
-  for (int i = 0; i < num_rows; i++)
-  {
-    for (int j = 0; j < num_cols; j++)
-    {
+void insert_grid_data(int **grid, const int num_rows, const int num_cols) {
+  for (int i = 0; i < num_rows; i++) {
+    for (int j = 0; j < num_cols; j++) {
       cin >> grid[i][j];
     }
   }
@@ -120,22 +113,17 @@ void insert_grid_data(int **grid, const int num_rows, const int num_cols)
   return;
 }
 
-int count_rad_robots(int **const grid, const int num_rows, const int num_cols)
-{
+int count_rad_robots(int **const grid, const int num_rows, const int num_cols) {
   int num_rad_robots = 0;
   int radius = 0; // robot's signal strength
 
-  for (int i = 0; i < num_rows; i++)
-  {
-    for (int j = 0; j < num_cols; j++)
-    {
+  for (int i = 0; i < num_rows; i++) {
+    for (int j = 0; j < num_cols; j++) {
       radius = grid[i][j];
-      if (radius != 0)
-      {
+
+      if (radius != 0) {
         if (is_isolated(grid, i, j, num_rows, num_cols, radius))
-        {
           num_rad_robots++;
-        }
       }
     }
   }
@@ -150,31 +138,23 @@ bool is_isolated(
   const int max_row,
   const int max_col,
   const int radius
-)
-{
+) {
   int count = 0; // number of robots within radius
   double distance = 0;
 
   // Check surrounding area within the radius of the point of interest
-  for (int i = point_row - radius; i <= point_row + radius; i++)
-  {
-    for (int j = point_col - radius; j <= point_col + radius; j++)
-    {
+  for (int i = point_row - radius; i <= point_row + radius; i++) {
+    for (int j = point_col - radius; j <= point_col + radius; j++) {
       // Only access memory allocated for the grid
-      if (i >= 0 && j >= 0 && i < max_row && j < max_col)
-      {
-        if (grid[i][j] != 0 && (i != point_row || j != point_col))
-        {
+      if (i >= 0 && j >= 0 && i < max_row && j < max_col) {
+        if (grid[i][j] != 0 && (i != point_row || j != point_col)) {
           distance = get_distance(point_col, point_row, j, i);
+
           if (distance <= radius)
-          {
             count++;
-          }
         }
         if (count >= NOT_ISOLATED_MIN)
-        {
           return false;
-        }
       }
     }
   }
@@ -182,7 +162,6 @@ bool is_isolated(
   return true;
 }
 
-double get_distance(const int x_1, const int y_1, const int x_2, const int y_2)
-{
+double get_distance(const int x_1, const int y_1, const int x_2, const int y_2) {
   return sqrt((x_1 - x_2) * (x_1 - x_2) + (y_1 - y_2) * (y_1 - y_2));
 }
