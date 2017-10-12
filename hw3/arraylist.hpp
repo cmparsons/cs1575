995397a539d70fb763a5e575c35bb3acd5bad18b
cmparsons <christianp130@gmail.com>
2017-10-12 16:16:38 -0500

    Add auxiliary functions and rest of basic mutator functions.

--- hw3/arraylist.hpp
+++ hw3/arraylist.hpp
@@ -11,7 +11,34 @@
 
 using namespace std;
 
-// MARK : Constructors/Destructors
+// MARK: Auxiliary Functions
+template <class T>
+void ArrayList<T>::grow()
+{
+  m_max *= 2;
+  T *new_list = new T[m_max];
+
+  for (int i = 0; i < m_size; i++)
+    new_list[i] = m_data[i];
+
+  delete[] m_data;
+  m_data = new_list;
+}
+
+template <class T>
+void ArrayList<T>::shrink()
+{
+  m_max /= 2;
+  T *new_list = new T[m_max];
+
+  for (int i = 0; i < m_size; i++)
+    new_list[i] = m_data[i];
+
+  delete[] m_data;
+  m_data = new_list;
+}
+
+// MARK: Constructors/Destructors
 template <class T>
 ArrayList<T>::~ArrayList()
 {
@@ -125,6 +152,75 @@ void ArrayList<T>::clear()
   m_size = m_max = 0;
   delete[] m_data;
   m_data = NULL;
+}
+
+template <class T>
+void ArrayList<T>::insert_back(const T& x)
+{
+  // Special case if list is not allocated
+  if (m_size == 0 && m_max == 0)
+    m_data = new T[++m_max];
+
+  m_data[m_size++] = x;
+
+  if (m_size == m_max)
+    grow();
+
+  return;
+}
+
+template <class T>
+void ArrayList<T>::insert(const T& x, int i)
+{
+  if (i < 0 || i >= m_size)
+  {
+    cout << "!-- ERROR : PANIC in ARRAYLIST!!.insert()  (index out of bounds)"
+         << endl;
+    return;
+  }
+
+  // Check before shifting to prevent accessing unallocated memory
+  if (++m_size == m_max)
+    grow();
+
+  // Shift elements right of i
+  for (int j = m_size - 2; j >= i; j--)
+    m_data[j + 1] = m_data[j];
+
+  m_data[i] = x;
+
+  return;
+}
+
+template <class T>
+void ArrayList<T>::remove(int i)
+{
+  if (m_size == 0)
+  {
+    cout << "!-- ERROR : PANIC in ARRAYLIST.remove()!!  (List is empty)"
+         << endl;
+    return;
+  }
+  if (i < 0 || i >= m_size)
+  {
+    cout << "!-- ERROR : PANIC in ARRAYLIST!!.remove()  (index out of bounds)"
+         << endl;
+    return;
+  }
+
+  // Don't shift elements if removing from the end
+  if (i < m_size - 1)
+  {
+    // Shift elements left of i
+    for (int j = i; j < m_size; j++)
+      m_data[j] = m_data[j + 1];
+  }
+  m_size--;
+
+  if (m_size < 0.25 * m_max)
+    shrink();
+
+  return;
 }
 
 #endif
