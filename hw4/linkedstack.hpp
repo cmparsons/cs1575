ee6c33d8ebe0f03b0627b7f9b5f47524b121310e
cmparsons <christianp130@gmail.com>
2017-10-18 21:02:06 -0500

    Add initial files for HW4

---
+++ hw4/linkedstack.hpp
@@ -0,0 +1,16 @@
+/*
+ * @author: Christian Parsons
+ * @section: B
+ * @due: October 23, 2017
+ * @description: Functions for templated linked stack.
+ * @file: linkedstack.h
+ * Implementing A.D.T. Stack
+ * @C - Galaxy Express Software
+ *
+ * Version 17.3.1
+ */
+
+ #ifndef LINKEDSTACK_HPP
+ #define LINKEDSTACK_HPP
+
+ #endif
