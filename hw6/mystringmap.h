0f8c2cef3297d62c06df956c65f54991ffac4726
cmparsons <christianp130@gmail.com>
2017-11-10 12:03:44 -0600

    Add initial files

---
+++ hw6/mystringmap.h
@@ -0,0 +1,18 @@
+/*
+ * @author: Christian Parsons
+ *
+ * @section: B
+ *
+ * @due: November 17, 2017
+ *
+ * @description: Implementation of an templated string map ADT.
+ *
+ * @file: mystringmap.h
+ *
+ */
+
+#ifndef MYSTRINGMAP_H
+#define MYSTRINGMAP_H
+
+#include "mystringmap.hpp"
+#endif
---
+++ hw6/mystringmap.hpp
@@ -0,0 +1,17 @@
+/*
+ * @author: Christian Parsons
+ *
+ * @section: B
+ *
+ * @due: November 17, 2017
+ *
+ * @description: Member functions for implementation of string map.
+ *
+ * @file: mystringmap.hpp
+ *
+ */
+
+#ifndef MYSTRINGMAP_HPP
+#define MYSTRINGMAP_HPP
+
+#endif
