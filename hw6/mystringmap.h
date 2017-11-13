28aee4cd9735fd60ad3252f7f6a16ca0229f46be
cmparsons <christianp130@gmail.com>
2017-11-12 19:15:33 -0600

    Add insert functionality

--- hw6/mystringmap.h
+++ hw6/mystringmap.h
@@ -1,18 +1,92 @@
 /*
- * @author: Christian Parsons
- *
- * @section: B
- *
- * @due: November 17, 2017
- *
- * @description: Implementation of an templated string map ADT.
- *
- * @file: mystringmap.h
- *
- */
+* @author: Christian Parsons
+*
+* @section: B
+*
+* @due: November 17, 2017
+*
+* @description: Implementation of an templated string map ADT.
+*
+* @file: mystringmap.h
+*
+*/
 
 #ifndef MYSTRINGMAP_H
 #define MYSTRINGMAP_H
+
+#include "abstractstringmap.h"
+
+template <class T>
+class KVPair
+{
+private:
+  string m_key;
+  T m_val;
+
+public:
+  KVPair() {}
+
+  KVPair(const string key, const T val) : m_key(key), m_val(val) {}
+
+  KVPair(const KVPair<T> &rhs)
+  {
+    m_key = rhs.m_key;
+    m_val = rhs.m_val;
+  }
+
+  void operator=(const KVPair<T> &rhs)
+  {
+    m_key = rhs.m_key;
+    m_val = rhs.m_val;
+  }
+
+  string key() const { return m_key; }
+
+  T value() const { return m_val; }
+
+  void set_key(const string &key) { m_key = key; }
+
+  void set_value(const T &value) { m_val = value; }
+};
+
+template <class T>
+class MyStringMap : public AbstractStringMap<T>
+{
+private:
+  int m_size;
+  int m_max;
+  KVPair<T> *m_hash_table;
+
+  string EMPTY;
+  string TOMB;
+
+  int hash(const string &key) const;
+
+  int double_hash(const string &key, const int i) const
+  {
+    return i * hash(key);
+  }
+
+  // Increase the size of the hash table and rehash records.
+  void grow();
+
+public:
+  MyStringMap();
+
+  ~MyStringMap() { clear(); }
+
+  void clear();
+
+  int size() const { return m_size; }
+
+  int max() const { return m_max; }
+
+  bool isEmpty() const { return m_size == 0; }
+
+  void insert(const string &key, const T &val);
+
+  void print() const;
+};
 
 #include "mystringmap.hpp"
 #endif
--- hw6/mystringmap.hpp
+++ hw6/mystringmap.hpp
@@ -14,4 +14,118 @@
 #ifndef MYSTRINGMAP_HPP
 #define MYSTRINGMAP_HPP
 
+#include <string>
+
+template <class T>
+MyStringMap<T>::MyStringMap()
+{
+  EMPTY = "-1";
+  TOMB = "-2";
+  m_size = 0;
+  m_max = 8;
+  m_hash_table = new KVPair<T>[m_max];
+  for (int i = 0; i < m_max; i++)
+    m_hash_table[i].set_key(EMPTY);
+}
+
+template <class T>
+void MyStringMap<T>::grow()
+{
+  int old_max = m_max;
+  m_max = (m_max << 1) + 1;
+  KVPair<T> *new_hash_table = new KVPair<T>[m_max]();
+  string key;
+  int home, pos, j;
+
+  for (int i = 0; i < m_max; i++)
+    new_hash_table[i].set_key(EMPTY);
+
+  for (int i = 0; i < old_max; i++)
+  {
+    key = m_hash_table[i].key();
+
+    // Rehash records in new hash table
+    if (key != EMPTY && key != TOMB)
+    {
+      pos = home = hash(key);
+      j = 1;
+      while (new_hash_table[pos].key() != EMPTY)
+      {
+        pos = (home + double_hash(key, j)) % m_max;
+        j++;
+      }
+      KVPair<T> new_record(key, m_hash_table[i].value());
+      new_hash_table[pos] = new_record;
+    }
+  }
+
+  delete[] m_hash_table;
+  m_hash_table = new_hash_table;
+  return;
+}
+
+template <class T>
+void MyStringMap<T>::clear()
+{
+  delete[] m_hash_table;
+  m_hash_table = NULL;
+  m_size = 0;
+  return;
+}
+
+template <class T>
+int MyStringMap<T>::hash(const string &key) const
+{
+  unsigned int hash = 0;
+  for (int i = 0; i < key.length(); i++)
+    hash = 31 * hash + key[i];
+
+  return hash % m_max;
+}
+
+template <class T>
+void MyStringMap<T>::insert(const string &key, const T &val)
+{
+  int home;
+  int pos = home = hash(key);
+  int i = 1;
+
+  // If key already exists, override the value
+  if (m_hash_table[pos].key() == key)
+  {
+    m_hash_table[pos].set_value(val);
+    return;
+  }
+
+  while (
+      m_hash_table[pos].key() != EMPTY &&
+      m_hash_table[pos].key() != TOMB &&
+      m_hash_table[pos].key() != key)
+  {
+    pos = (home + double_hash(key, i)) % m_max;
+    i++;
+  }
+  KVPair<T> new_record(key, val);
+  m_hash_table[pos] = new_record;
+
+  // Increase size of hash table if size reaches past load factor 3/4 max
+  if (++m_size >= 0.75 * m_max)
+    grow();
+
+  return;
+}
+
+template <class T>
+void MyStringMap<T>::print() const
+{
+  string key;
+  for (int i = 0; i < m_max; i++)
+  {
+    key = m_hash_table[i].key();
+    if (key != EMPTY && key != TOMB)
+      cout << "< " << key << ", " << m_hash_table[i].value() << " >" << endl;
+  }
+  return;
+}
+
 #endif
